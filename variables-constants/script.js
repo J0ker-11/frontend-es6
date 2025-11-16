4f76f6ec413c522221bc406510417d64bb6997d7
J0ker11 <primetech444@gmail.com>
2025-11-17 00:00:37 +0100

    feat: declared variables

---
+++ variables-constants/script.js
@@ -0,0 +1,15 @@
+
+// Variables 
+
+let userName = " Joker";
+const pi = 3.14159;
+
+// Constants can be declared using 'const'
+const gravity = 9.81; // m/s^2
+
+// Variables can be assigned and reassigned
+userName = "Batman";
+
+console.log(userName); // Output Batman
+console.log(pi); // Output 3.14159
+console.log(gravity); // Output 9.81
